982a27fe052914008ddedb38a592fbd489a4557d
Your Name <you@example.com>
2019-04-12 16:38:43 -0400

    starting to come together

---
+++ main.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const RssFeedEmitter = require('rss-feed-emitter');
+const mysql      = require('mysql');
+let feeder = new RssFeedEmitter();
+let pool = mysql.createPool({
+  connectionLimit : 10,
+  host     : 'localhost',
+  user     : 'root',
+  password : 'ifu4jb2a',
+  database : 'parser'
+});
+
+const addFeeds = () => {
+    let query = pool.query('SELECT * FROM sources');
+    query
+        .on('error', function (err) {
+            console.log(err);
+        })
+        .on('result', function (row) {
+            feeder.add({
+                url: row.url
+            });
+        })
+        .on('end', function () {
+            // anything?
+            console.log("feeds added: ");
+            let feedList = feeder.list();
+            for (var i = 0; i < feedList.length; i++) {
+                console.log(feedList[i].url);
+            }
+            //for (var feed in feedList) { Doesnt work, odd.
+                //console.log(feed.url);
+            //}
+        });
+}
+
+feeder.on('new-item', function(item) {
+    console.log("article found: " + item.title);
+    let sql = 'select * from articles where title = ?';
+	let inserts = [item.title];
+    let sql = mysql.format(sql, inserts);
+    let query = pool.query(sql, (error, results, fields) => {
+        if (results.length < 1) {
+            let pubdate = null;
+            let now = new Date().toISOString();
+            if (item.pubdate && item.pubdate != '') {
+                pubdate = item.pubdate;
+            } else if (item.pubDate && item.pubDate != '') {
+                pubdate = item.pubDate;
+            } else if (item.date && item.date != '') {
+                pubdate = item.date;
+            }
+            // "insert into titles (title,summary,link,published,timestamp,runtime,src,feedname,currentflag)"            
+            var post  = {title: item.title, summary: item.summary, link: item.origlink, published: item.pubdate, timestamp: now, runtime: now, src: '', feedname: feed.meta.title, currentflag: 'y'};
+            var query = pool.query('INSERT INTO articles SET ?', post, function (error, results, fields) {
+                if (error) throw error;
+            });
+        } else {
+            console.log("article already added: " + item.title);
+        }
+    });
+})
+
+addFeeds();
+
+app.get('/', (req, res) => 
+    {
+        var result = selectOne(res);
+    }
+);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const selectOne = (res) => {
+    let result = [];
+    let query = pool.query('SELECT * FROM articles');
+    query
+        .on('error', function (err) {
+            console.log(err);
+        })
+        .on('result', function (row) {
+            result.push(JSON.stringify(row));
+        })
+        .on('end', function () {
+            selectCount(result,res);
+        });
+};
+
+const selectCount = (result, res) => {
+    let query = pool.query('SELECT count(*) FROM articles');
+    query
+        .on('error', function (err) {
+            console.log(err);
+        })
+        .on('result', function (row) {
+            result.push(JSON.stringify(row));
+        })
+        .on('end', function () {
+            res.send(result);
+        });
+};
