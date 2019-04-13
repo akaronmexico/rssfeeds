d74000041daace959df42fe3aac4b9eddcfbbe1e
Your Name <you@example.com>
2019-04-13 01:18:06 -0400

    output

--- output.js
+++ output.js
@@ -1,42 +1,38 @@
 const fs = require('fs');
-const mysql      = require('mysql');
-let pool = mysql.createPool({
-  connectionLimit : 10,
-  host     : 'localhost',
-  user     : 'root',
-  password : 'ifu4jb2a',
-  database : 'parser'
-});
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('db.sqlite','OPEN_READWRITE');
 
-const outputLoop = async (error, results, fields) => {
+const outputLoop = async (error, results) => {
 	if (error) throw error;
 	for (let partner of results) {
 		partner = partner.partner;
 		let writeStream = fs.createWriteStream('output/' + partner + '.csv');
 		let sql = 'select * from ' + partner;
-		var query = pool.query(sql, function (error, results, fields) {
-			if (error) throw error;
-			let output = 'Target country | Keyword(s) | Title | Summary | Link | Published | Compiled | Source | feedname'
-			writeStream.write(output, 'utf8');
-			writeStream.write('\n', 'utf8');
-			for (let article of results) {
-				// Target country | Keyword(s) | Title | Summary | Link | Published | Compiled | Source | feedname
-				output = article.target + ' | ' + article.keywords + ' | ' + article.title + ' | ' + article.summary + ' | ' +  article.link
-						 + ' | ' + article.published + ' | ' + article.runtime + ' | ' + article.src + ' | ' + article.feedname;
-				writeStream.write(output, 'utf8');
-				writeStream.write('\n', 'utf8');
-			}
-			writeStream.on('finish', () => {
-				console.log('wrote all data to file');
-			});
-			writeStream.end();
-		});
+    let query = db.all('SELECT * FROM partnerdata where partner = ?', [partner], function(err,rows){
+      if (err) console.log(err);
+      else {
+        let output = 'Target country | Keyword(s) | Title | Summary | Link | Published | Compiled | Source | feedname'
+        writeStream.write(output, 'utf8');
+        writeStream.write('\n', 'utf8');
+        for (let article of rows) {
+          // Target country | Keyword(s) | Title | Summary | Link | Published | Compiled | Source | feedname
+          output = article.target + ' | ' + article.keywords + ' | ' + article.title + ' | ' + article.summary + ' | ' +  article.link
+              + ' | ' + article.published + ' | ' + article.runtime + ' | ' + article.src + ' | ' + article.feedname;
+          writeStream.write(output, 'utf8');
+          writeStream.write('\n', 'utf8');
+        }
+        writeStream.on('finish', () => {
+          console.log('wrote all data to file');
+        });
+        writeStream.end();
+      }
+    });
 	}
 };
 
 const createOutput = async () => {
   let sql = 'select distinct partner from partners';
-  let query = pool.query(sql, outputLoop);
+  let query = db.all(sql, outputLoop);
 }
 
 exports.createOutput = createOutput;
